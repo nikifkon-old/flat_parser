3b1a48ef8fff00b0a1c22ddd275f1d445e4f7e50
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-01-06 13:14:59 +0500

    implement sync parser

--- main.py
+++ main.py
@@ -1,22 +1,95 @@
+import json
+from time import sleep
 from web_parser.parser import Task, TaskManager
 
-URL = ""
+URL = "https://www.avito.ru/ekaterinburg/kvartiry/prodam/vtorichka?f=549_5696-5697-5698-5699-5700-5701-11018-11019-11020-11021"
 
 
 class GettingPageUrlList(Task):
-    def parse(self, bs):
-        pass
+    """ Get list of items urls for parse """
+    scroll_sleep_time = 0.5
+    file_path = 'links.txt'
+
+    def prepare(self, driver):
+        # TODO: also click load more button
+        last = driver.execute_script("return document.body.scrollHeight")
+        new = None
+        while last != new:
+            last = new
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            sleep(self.scroll_sleep_time)
+            new = driver.execute_script("return document.body.scrollHeight")
+            assert new is not None
+
+    def parse(self, driver):
+        container = driver.find_element_by_xpath(".//*[@data-marker='items/list']")
+        links_elements = container.find_elements_by_xpath("//*[@data-marker='item/link']")
+        links = [link.get_property("href") for link in links_elements]
+        return links
 
     def save_data(self, data):
-        pass
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            for link in data:
+                file.write(link + '\n')
+
+    def get_links(self):
+        # TODO: catch io errors
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            return file.read().split('\n')
+
+
+class GettingPageInfo(Task):
+    """ parse item """
+    total_area_label = "Общая площадь, м²"
+    house_type_label = "Тип дома"
+    address_label = "Адрес"
+    floor_label = "Этаж"
+    file_path = "data.json"
+
+    def parse(self, driver):
+        container = driver.find_element_by_xpath(".//*[@data-marker='item-container']")
+        price = container.find_element_by_xpath("//*[@data-marker='item-description/price']").text
+        prop_list = container.find_element_by_xpath("//*[@data-marker='item-properties/list']")
+
+        total_area = self.get_props_item_text(prop_list, self.total_area_label)
+        house_type = self.get_props_item_text(prop_list, self.house_type_label)
+        address = self.get_props_item_text(prop_list, self.address_label)
+        floor = self.get_props_item_text(prop_list, self.floor_label)
+
+        data = {
+            'price': price,
+            'address': address,
+            'total_area': total_area,
+            'house_type': house_type,
+            'floor': floor
+        }
+        return data
+
+    def get_props_item_text(self, prop_list, label):
+        item = prop_list.find_element_by_xpath(
+            "//*[.='%s']/../*[2]" % label
+        )
+        return item.text
+
+    def save_data(self, data):
+        with open(self.file_path, 'a', encoding='utf-8', newline="") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+            file.write('\n')
 
 
 def main():
-    tm = TaskManager()
-    t1 = GettingPageUrlList("get_urls", URL)
-    tm.add_task(t1)
+    manager = TaskManager()
+    getting_links_task = GettingPageUrlList("get_links", [URL])
+    manager.add_task(getting_links_task)
 
-    tm.run_all()
+    manager.run_by_name("get_links")
+
+    items_links = getting_links_task.get_links()
+    parse_item_task = GettingPageInfo("parse_url", items_links)
+    manager.add_task(parse_item_task)
+
+    manager.run_by_name("parse_url")
+
 
 if __name__ == '__main__':
     main()
--- web_parser/parser.py
+++ web_parser/parser.py
@@ -1,47 +1,99 @@
-class Parser:
-    headers = None
-
-    def __init__(self, url):
-        self.url = url
-
-    def get_html(self):
-        pass
-
-    def parse_html(self):
-        pass
+import os
+from functools import cached_property
+from random import choice
+from selenium import webdriver
 
 
-class Task(Parser):
-    status = "" # pending, running, successed, failed
+class Driver(webdriver.Chrome):
+    def __init__(self, *args, **kwargs):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.useragent_list_path = os.path.join(current_dir, "mobile_useragents.txt")
 
-    def __init__(self, name, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        options = webdriver.ChromeOptions()
+        # options.add_argument('headless')
+        options.add_argument("user-agent=%s" % self.get_random_useragent())
+        super().__init__(*args, options=options, **kwargs)
+
+    def get_random_useragent(self):
+        with open(self.useragent_list_path, 'r') as file:
+            ua_list = file.read().splitlines()
+            return choice(ua_list)
+
+STATUSES = [
+    'pending', 'running', 'successed', 'failed'
+]
+
+class Task():
+    def __init__(self, name, urls):
         self.name = name
+        self.urls = urls
+        self._status = "pending"
 
-    def parse(self, bs):
-        """ Abstract method received html as once argument,
+    @cached_property
+    def driver(self):
+        return Driver()
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, new):
+        if new in STATUSES:
+            self._status = new
+        else:
+            raise Exception('Status %s is not in allowed statuses. '\
+                            'Maybe you mean one of them: [%s]' % (new, ', '.join(STATUSES)))
+
+
+    def prepare(self, driver):
+        """ Abstract method called before `parse` wait for a loading
+        """
+
+    def parse(self, driver):
+        """ Abstract method received webdriver as once argument,
         and returns data used in `save_data` method """
-        pass
+        raise NotImplementedError('parse method must be overrided')
 
     def save_data(self, data):
         """ Abstract method received data from `parse` method,
         and saves it in to a file """
-        pass
-    
+        raise NotImplementedError('save_data method must be overrided')
+
     def run(self):
-        pass
+        for url in self.urls:
+            self.driver.get(url)
+            self.status = "running"
+            try:
+                self.prepare(self.driver)
+                data = self.parse(self.driver)
+                self.save_data(data)
+            except Exception as error:
+                pass
+            finally:
+                self.status = "successed" if error is None else "failed"
+        self.driver.quit()
+
+
+    def __repr__(self):
+        return "<%s: name=%s, status=%s>" % (self.__class__.__name__, self.name, self.status)
 
 
 class TaskManager():
-    pending_tasks = []
-    running_tasks = []
-    finished_tasks = []
+    tasks = {}
 
     def add_task(self, task):
-        pass
+        self.tasks[task.name] = task
 
     def run_all(self):
-        pass
+        for task in self.tasks.values():
+            task.run()
 
     def run_by_name(self, name):
-        pass
+        if name in self.tasks:
+            self.tasks[name].run()
+        else:
+            raise Exception('Task with name: `%s` does not exist' % name)
+
+    def __repr__(self):
+        return "<%s>" % self.__class__.__name__
