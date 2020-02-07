eb1107c56d5f03db27e8fa07e2f13c5cc4a88a98
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-02-07 16:42:39 +0500

    refactoring main func

--- main.py
+++ main.py
@@ -25,69 +25,77 @@ def read_config(config_file):
 
 
 def get_prev_data(path):
+    if path is None:
+        sys.exit(f'No input file passed to console or in config file: {CONFIG_FILE}')
     if os.path.exists(path):
         with open(path, encoding='utf-8') as file:
             dict_reader = csv.DictReader(file)
             data = list(dict_reader)
         return data
+    else:
+        print(f'Input file: {path} does not exist. '
+              'Try specify it in config file or passed it as a console argument.')
+
+
+class NoUrlException(Exception):
+    def __init__(self, name):
+        self.name = name
 
 
 def main():
     start_time = time()
     config = read_config(CONFIG_FILE)
+    flat_parser_output = config['main'].get('default_output_flat_parsers')
 
     if len(sys.argv) >= 2:
+        name = sys.argv[1]
         manager = TaskManager()
-        if len(sys.argv) >= 3:
-            output_file = sys.argv[2]
-        else:
-            output_file = None
-        if sys.argv[1] == 'avito':
-            url = config['avito'].get('url')
-            scroll_count = int(config['avito'].get('scroll_count'))
-            if url is None:
-                sys.exit('Config has not avito url')
-            task = manager.create_task(GettingAvitoFlatInfo, "avito", url,
-                                       output_file=output_file,
-                                       scroll_count=scroll_count)
-            task.run()
-        elif sys.argv[1] == 'jula':
-            url = config['jula'].get('url')
-            scroll_count = int(config['jula'].get('scroll_count'))
-            if url is None:
-                sys.exit('Config has not jula url')
-            task = manager.create_task(GettingJulaFlatInfo, "jula", url,
-                                       output_file=output_file,
-                                       scroll_count=scroll_count)
-            task.run()
-        elif sys.argv[1] == 'upn':
-            url = config['upn'].get('url')
-            page_count = int(config['upn'].get('page_count'))
-            if url is None:
-                sys.exit('Config has not upn url')
-            task = manager.create_task(GettingUPNFlatInfo, "upn", url,
-                                       output_file=output_file,
-                                       page_count=page_count)
-            task.run()
-        elif sys.argv[1] == 'domaekb':
-            if len(sys.argv) >= 4:
-                output_file = sys.argv[3]
-            else:
-                output_file = None
-            if len(sys.argv) >= 3:
-                input_file = sys.argv[2]
-            else:
-                input_file = 'flat_info.csv'
+        output_file = sys.argv[2] if len(sys.argv) >= 3 else flat_parser_output
 
-            prev_data = get_prev_data(input_file)
-            if prev_data:
-                tasks = GettingHouseInfo.create_tasks_from_addresses(prev_data,
-                                                                     output_file=output_file)
-                with ProcessPoolExecutor(os.cpu_count()) as executor:
-                    executor.map(run_task, tasks)
-        else:
-            print(f'`{sys.argv[1]}` is not valid parser name')
-            sys.exit(1)
+        try:
+            if name == 'avito':
+                url = config['avito'].get('url')
+                scroll_count = int(config['avito'].get('scroll_count'))
+                if url is None:
+                    raise NoUrlException(name)
+                task = manager.create_task(GettingAvitoFlatInfo, "avito", url,
+                                           output_file=output_file,
+                                           scroll_count=scroll_count)
+                task.run()
+            elif name == 'jula':
+                url = config['jula'].get('url')
+                scroll_count = int(config['jula'].get('scroll_count'))
+                if url is None:
+                    raise NoUrlException(name)
+                task = manager.create_task(GettingJulaFlatInfo, "jula", url,
+                                           output_file=output_file,
+                                           scroll_count=scroll_count)
+                task.run()
+            elif name == 'upn':
+                url = config['upn'].get('url')
+                page_count = int(config['upn'].get('page_count'))
+                if url is None:
+                    raise NoUrlException(name)
+                task = manager.create_task(GettingUPNFlatInfo, "upn", url,
+                                           output_file=output_file,
+                                           page_count=page_count)
+                task.run()
+            elif name == 'domaekb':
+                output_file = sys.argv[3] if len(sys.argv) >= 4 else None
+                input_file = sys.argv[2] if len(
+                    sys.argv) >= 3 else flat_parser_output
+
+                prev_data = get_prev_data(input_file)
+                if prev_data:
+                    tasks = GettingHouseInfo.create_tasks_from_addresses(prev_data,
+                                                                         output_file=output_file)
+                    with ProcessPoolExecutor(os.cpu_count()) as executor:
+                        executor.map(run_task, tasks)
+            # No match parser name
+            else:
+                sys.exit(f'`{name}` is not valid parser name')
+        except NoUrlException as exc:
+            sys.exit(f'{exc} url is not found in cofing file: {CONFIG_FILE}')
     else:
         print('Please pass parser name')
     print(f"Time: {round(time() - start_time, 2)} sec.")
