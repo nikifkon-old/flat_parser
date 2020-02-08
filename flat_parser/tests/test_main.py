c7fdfff2a597ba0ddc5c7173721aa94e86045774
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-02-08 13:02:37 +0500

    --amend

--- flat_parser/tests/test_main.py
+++ flat_parser/tests/test_main.py
@@ -2,13 +2,13 @@ from flat_parser.main import get_prev_data
 
 
 def test_get_prev_data_dont_exist():
-    path = 'tests/test_prev_data_dont_exist.csv'
+    path = 'flat_parser/tests/test_prev_data_dont_exist.csv'
     data = get_prev_data(path)
     assert data is None
 
 
 def test_get_prev_data_empty():
-    path = 'tests/test_prev_data_empty.csv'
+    path = 'flat_parser/tests/test_prev_data_empty.csv'
     data = get_prev_data(path)
     assert data == []
 
@@ -18,7 +18,7 @@ def test_get_prev_data_success():
         {'price': '3700000', 'address': 'Екатеринбург, 62/2 улица Уральская', 'floor': '4'},
         {'price': '3700001', 'address': 'Екатеринбург, 62/22 улица Уральская', 'floor': '5'},
     ]
-    path = 'tests/test_prev_data_success.csv'
+    path = 'flat_parser/tests/test_prev_data_success.csv'
     data = get_prev_data(path)
     assert data is not None
     assert data == expected
