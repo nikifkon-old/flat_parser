20dffbad4509e00548d40d9f33a9b63a02f5d627
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-02-18 15:18:18 +0500

    add day support

--- flat_parser/data_modify/tests/test_utils.py
+++ flat_parser/data_modify/tests/test_utils.py
@@ -12,11 +12,13 @@ def test_get_meter_price(data, meter_price):
 
 
 @pytest.mark.parametrize('text, time', [
+    ('', 0),
+    (None, None),
     ('45 мин.', 45),
     ('1 ч. 20 мин.', 80),
     ('2 ч.', 120),
-    ('', 0),
-    (None, None)
+    ('1 д.', 1440),
+    ('2 д. 2 ч. 10 мин.', 3010)
 ])
 def test_get_time_in_minutes_by_text(text, time):
     assert time == get_time_in_minutes_by_text(text)
--- flat_parser/data_modify/utils.py
+++ flat_parser/data_modify/utils.py
@@ -13,12 +13,19 @@ def get_meter_price(data: dict) -> str:
 def get_time_in_minutes_by_text(text):
     try:
         strp = []
+        need_date = False
+        if 'д.' in text:
+            strp.append('%d д.')
+            need_date = True
         if 'ч.' in text:
             strp.append('%H ч.')
         if 'мин.' in text:
             strp.append('%M мин.')
         time = datetime.datetime.strptime(text, ' '.join(strp))
-        return time.minute + time.hour * 60
+        result = time.minute + time.hour * 60
+        if need_date:
+            result += time.day * 1440
+        return result
     except TypeError:
         print(f'[Error] Unable to get time in minutest - {text}')
         return None
